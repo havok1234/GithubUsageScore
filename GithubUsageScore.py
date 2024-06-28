0297393986a03cd5fc8003108088347548fa0163
Advait Chabukswar <advaitchabukswar@Advaits-Air.cgocable.net>
2024-06-27 23:41:03 -0400

    Initial Commit with code

---
+++ GithubUsageScore.py
@@ -0,0 +1,82 @@
+# This code checks the Github Usage stats and generates a usage score.
+# Scoring criteria: []
+# https://api.github.com/users/josevalim/events
+#TODO: Add Validation for correct user value
+#TODO: Add Logging
+
+import requests
+import sys
+
+class GithubUsageScore:
+    ## Variables
+    eventScores = {"PullRequestEvent": 5, "PushEvent": 4, "IssueCommentEvent": 3, "CreateEvent": 2, "Other": 1} # Scores for different event types.
+    baseUrl = "https://api.github.com/users/"
+    scores = []
+
+    # Read user(s) from input.txt
+    def getUsers(self):
+        try:
+            f = open("./input.txt", "r")
+            users = f.read().split(",")
+        except OSError:
+            print("Could not open/read file: input.txt")
+            sys.exit()
+        
+        if not users:
+            raise Exception("No Input provided")
+        return users
+        
+
+    # Call github api to retrieve user event usage metadata
+    def getEvents(self, user):
+        try: 
+            response = requests.get(f"{self.baseUrl}{user}/events")
+            events = []
+            try:
+                data = response.json()
+                for obj in data:
+                    events.append(obj["type"])
+            except requests.JSONDecodeError:
+                self.scores.append(f"Unexpected error while fetching Event Data for {user}")
+        except requests.RequestException as e:
+            self.scores.append(f"Unexpected error while fetching Event Data for {user}")
+            
+        return events
+            
+    # Calculate github usage score for a user by fetching their events
+    def calculateScore(self, user):
+        events = self.getEvents(user)
+        score = 0
+        #iterate over events to calculate score
+        for event in events:
+            if event in self.eventScores:
+                score += self.eventScores[event]
+            else:
+                score += self.eventScores["Other"]
+        self.scores.append(f"{user}'s github usage score is {score}")
+    
+    # Print github usage score(s) for a user(s)
+    def printScores(self):
+        for score in self.scores:
+            print(score)
+        
+    # Check github usage score(s) for a user(s)
+    def checkScore(self):
+        # Get users
+        users = self.getUsers()
+            
+        # Calculate Score for each user
+        for user in users:
+            if user.strip():
+                self.calculateScore(user.strip())
+        
+        # Print Score(s)
+        self.printScores()
+        
+    
+def main():
+    g = GithubUsageScore()
+    g.checkScore()
+    
+if __name__ == '__main__':
+    main()
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+## Description
+This script calculates the Github Usage Scores for single or multiple users.
+
+## Input
+Provide a comma-separated user values using the input.txt file to test the script. 
+    E.g. advait, evan
+
+## Output
+The script output's each user's score on a single line
+    E.g. advait's github usage score is 0
+         evan's github usage score is 24
